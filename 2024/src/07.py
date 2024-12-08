db2705ea67abceda65e6b4c10008b16c327f1ee6
Ben Wagner <bewagner@davidson.edu>
2024-12-08 16:13:54 -0500

    Day 7 complete

--- 2024/src/07.py
+++ 2024/src/07.py
@@ -13,12 +13,15 @@ def get_operator(operator):
     return op
 
 
-def validate(op):
+def validate(op, two=False):
 
     target = op[0]
 
     if len(op) == 3:
-        if target == op[1]*op[2] or target == op[1]+op[2]:
+        if two:
+            if target == op[1]*op[2] or target == op[1]+op[2] or target == int(str(op[1])+str(op[2])):
+                return target
+        elif target == op[1]*op[2] or target == op[1]+op[2]:
             return target
         return 0
 
@@ -28,16 +31,19 @@ def validate(op):
         return 0
     elif target < total * op[2]:
         total += op[2]
-        return validate([target, total] + op[3:])
-    else:
-        if validate([target, total*op[2]] + op[3:]) == target:
+        return validate([target, total] + op[3:], two=two)
+    elif target < int(str(total) + str(op[2])) and two:
+        if validate([target, total*op[2]] + op[3:], two=two) == target:
             return target
         else:
-            return validate([target, total+op[2]] + op[3:])
-
-
-
-
+            return validate([target, total+op[2]] + op[3:], two=two)
+    else:
+        if validate([target, int(str(total)+str(op[2]))] + op[3:], two=two) == target and two:
+            return target
+        elif validate([target, total*op[2]] + op[3:], two=two) == target:
+            return target
+        else:
+            return validate([target, total+op[2]] + op[3:], two=two)
 
 def part_one(input):
 
@@ -50,6 +56,17 @@ def part_one(input):
     return
 
 
+def part_two(input):
+
+    sum = 0
+    for operator in input:
+        op = get_operator(operator)
+        sum += validate(op, two=True)
+
+    print(sum)
+    return
+
+
 if __name__ =='__main__':
 
     FILE_PATH = "./puzzles/07.txt"
@@ -57,3 +74,4 @@ if __name__ =='__main__':
         text = f.readlines()
 
     part_one(text)
+    part_two(text)
