ef4c3047caf97b51aa420b3d36ee40b49e7246ec
Ben Wagner <bewagner@davidson.edu>
2024-12-08 17:59:39 -0500

    Initial state

---
+++ 2024/src/08.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("../puzzles")
+
+
+def part_one(text):
+    return
+
+if __name__ == '__main__':
+    
+    FILE_PATH = "./puzzles/08_ex.txt"
+    with open(FILE_PATH, 'r') as f:
+        text = f.readlines()
+
+    part_one(text)
