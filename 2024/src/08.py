18ee358f66472ae479edcfd907f65abae46d0bfe
Ben Wagner <bewagner@davidson.edu>
2024-12-09 01:17:12 -0500

    in progress

--- 2024/src/08.py
+++ 2024/src/08.py
@@ -2,8 +2,38 @@ import sys
 sys.path.append("../puzzles")
 
 
-def part_one(text):
+def process(text):
+    for i, l in enumerate(text):
+        if '\n' in l:
+            text[i] = l[:-1]
+    return text
+
+
+def bounds_check(r, c, w, h):
+    if r < 0 or c < 0:
+        return False
+    elif r > h or c > w:
+        return False
+    return True
+
+
+def is_node(r, c, map):
     return
+
+def part_one(input):
+
+    height = len(input)
+    width = len(input[0])
+
+    n_nodes = 0
+    for i in range(height):
+        for j in range(width):
+            if is_node(i, j, input):
+                n_nodes += 1
+
+    print(n_nodes)
+
+    
 
 if __name__ == '__main__':
     
@@ -11,4 +41,5 @@ if __name__ == '__main__':
     with open(FILE_PATH, 'r') as f:
         text = f.readlines()
 
+    text = process(text)
     part_one(text)
