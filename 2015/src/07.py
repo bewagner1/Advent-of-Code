5bad4a0849645a4f77b324639a9cba4ae45424f7
Ben Wagner <benjaminwagner21@gmail.com>
2025-12-12 20:57:00 -0800

    day7 complete

--- 2015/src/07.py
+++ 2015/src/07.py
@@ -6,8 +6,44 @@ from argparse import ArgumentParser
 from pprint import pprint
 
 
-def find_value(wire):
-    return
+def find_value(circuits, wire, cache=None):
+    if cache is None:
+        cache = {}
+    
+    # Check cache first
+    if wire in cache:
+        return cache[wire]
+    
+    # Base case: literal number
+    if wire.isdigit():
+        return int(wire)
+    
+    # Get the expression for this wire
+    expr = circuits[wire]
+    
+    # Parse and compute based on operation
+    if 'AND' in expr:
+        l, r = expr.split(' AND ')
+        result = find_value(circuits, l, cache) & find_value(circuits, r, cache)
+    elif 'OR' in expr:
+        l, r = expr.split(' OR ')
+        result = find_value(circuits, l, cache) | find_value(circuits, r, cache)
+    elif 'RSHIFT' in expr:
+        l, r = expr.split(' RSHIFT ')
+        result = find_value(circuits, l, cache) >> find_value(circuits, r, cache)
+    elif 'LSHIFT' in expr:
+        l, r = expr.split(' LSHIFT ')
+        result = find_value(circuits, l, cache) << find_value(circuits, r, cache)
+    elif 'NOT' in expr:
+        l = expr.split('NOT ')[-1]
+        result = ~find_value(circuits, l, cache) & 0xFFFF  # Mask to 16 bits
+    else:
+        # Direct wire reference
+        result = find_value(circuits, expr, cache)
+    
+    # Cache and return
+    cache[wire] = result
+    return result
 
 
 def main(puzzle, part_two=False):
@@ -18,7 +54,13 @@ def main(puzzle, part_two=False):
             x = ln.rstrip('\n').split(' -> ')
             circuits[x[-1]] = x[0]
 
-    print(f"The value in wire a is {find_value('a')}")
+    a = find_value(circuits, 'a')
+
+    if not part_two:
+        print(f"The value in wire a is {a}")
+    else:
+        circuits['b'] = str(a)
+        print(f"The value in wire a is {find_value(circuits, 'a')}")
 
     return
 
